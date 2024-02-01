72ce8fb8bae5fdc3f1f82a074052291cb6ebd91c
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-02-01 18:57:28 -0500

    End of episodes

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -2,4 +2,12 @@ var x; // Declare x
 x = 5; // Assign 5 to x
 
 elem = document.getElementById("demo"); 
-elem.innerHTML = x;                    
+elem.innerHTML = x;     
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };               
