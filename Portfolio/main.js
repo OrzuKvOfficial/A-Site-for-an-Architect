c845eb81b87a638cb1d3510c59aadfc6cb77b0df
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-02-06 16:25:25 -0500

    just code

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -192,3 +192,10 @@ function myFunction(xml) {
   }
   document.getElementById("demo").innerHTML = table;
 }
+function sum(...args) {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
+
+let x = sum(4, 9, 16, 25, 29, 100, 66, 77);
