53a50088c637e3500fc914df39b2be358fd7941b
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-02-07 17:57:07 -0500

    There is only a change in js

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -210,3 +210,6 @@ const price2 = 6;
 const PI = 3.141592653589793;
 PI = 3.14;      // This will give an error
 PI = PI + 10;   // This will also give an error
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
